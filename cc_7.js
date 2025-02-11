e40b3914f523db6e843555380a1155af6e65e2e3
HIRUN <hirundesilva97@gmail.com>
2025-02-11 10:33:10 -0500

    Task 1 - Customer Invoice Calculation

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,8 @@
-//Task 1
+//Task 1 - Customer Invoice Calculation
+function calculateInvoice(subTotal, taxRate, discount) { //function calculateInvoice
+    let total = (subTotal + (subTotal * taxRate)) - discount //formula total
+    console.log(`Total Invoice: $${total}`)   //logging total invoice for function
+}
+console.log("---Task 1---")
+calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
+calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"  
