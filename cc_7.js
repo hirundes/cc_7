c11588c625f14cf3b2dbdf3005b3036a426e415c
HIRUN <hirundesilva97@gmail.com>
2025-02-11 13:07:18 -0500

    Task 5 - Business Loan Interest Calculation

--- cc_7.js
+++ cc_7.js
@@ -46,6 +46,14 @@ calculateShippingCost(10, "USA", true); // Expected output: "Shipping Cost: $20.
 calculateShippingCost(5, "Canada", false); // Expected output: "Shipping Cost: $13.50"
 
 
+//Task 5 - Business Loan Interest Calculation
+function calculateLoanInterest(principle, rate, years) {    //function calculateLoanInterest
+    let interest = (principle * rate * years).toFixed(2);      //formula interest w/ 2 decimal places
+    console.log(`Total Interest: $${interest}`);     //logged interest
+}
+console.log("---Task 5---");
+calculateLoanInterest(1000, 0.05, 3); // Expected output: "Total Interest: $150.00"
+calculateLoanInterest(5000, 0.07, 5); // Expected output: "Total Interest: $1750.00"
 
 
 
