528805df29476dc00620a5fbc5db7ccda36c007d
HIRUN <hirundesilva97@gmail.com>
2025-02-11 12:25:03 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -3,7 +3,7 @@ function calculateInvoice(subTotal, taxRate, discount) {    //function calculate
     let total = (subTotal + (subTotal * taxRate)) - discount;    //formula total
     console.log(`Total Invoice: $${total}`);   //logging total invoice for function
 };
-console.log("---Task 1---")
+console.log("---Task 1---");
 calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"  
 
@@ -13,7 +13,24 @@ const calculateHourlyWage = function(salary, hoursPerWeek) {    // function expr
     let hourlyWage = (salary / (hoursPerWeek * 52));     //formula hourlyWage
     console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);   //logging hourlyWage
 };
-console.log("---Task 2---")
+console.log("---Task 2---");
 calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
 calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
+
+//Task 3 - Customer Loyalty Discount
+const calculateLoyaltyDiscount = (amount, years) => {   //arrow function calculateLoyaltyDiscount
+    let loyaltyDiscount = 0; 
+    if (years >= 5) loyaltyDiscount = amount * 0.15;    //applying discount with if / else statements
+    else if (years >= 3) loyaltyDiscount = amount * 0.10;
+    else loyaltyDiscount = amount * 0.05;
+    console.log(`Discounted Price: $${amount - loyaltyDiscount}`);   //logging loyaltyDiscount
+
+};
+console.log("---Task 3---");
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00" 
+
+
+
 
